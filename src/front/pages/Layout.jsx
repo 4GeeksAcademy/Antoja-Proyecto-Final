3157c7fe8726e5bd4ec88f56b0d7a93818460d7c
Javiera Vega <javieravegareyes7@gmail.com>
2025-06-20 22:50:28 +0000

    creation landing page

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,13 +2,20 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import { CarouselProducts } from "../components/CarouselProducts"
+import { AboutUs } from "../components/AboutUs"
+import { Contacts } from "../components/Contacts"
+import { MapsUbication } from "../components/MapsUbication"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
             <Navbar />
-                <Outlet />
+            <CarouselProducts />
+            <AboutUs />
+            <Contacts />
+            <MapsUbication />
             <Footer />
         </ScrollToTop>
     )
