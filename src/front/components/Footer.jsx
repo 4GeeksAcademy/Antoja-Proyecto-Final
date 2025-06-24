a281376c14be7cde3206fcb61a1a9bc3648ff8bd
Javiera Vega <javieravegareyes7@gmail.com>
2025-06-24 21:02:11 +0000

    guarde mis cambios antes de cambiar a la rama develop

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -2,27 +2,36 @@ import { Link } from "react-router-dom"
 
 export const Footer = () => {
 	return (
-		<div className="container-footer p-3 ">
-			<div className="row col-12 col-md-6">
+		<div className="container-footer p-2 ">
+			<div className="row col-12">
 				<div className="text-center mt-1 text-dark">
 					<h2>Contáctanos</h2>
 				</div>
+				<div className="info-footer">
+					<div className="information-contacts">
+						<div className="info-icons">
+							<i className="fa-solid fa-location-dot"></i>
+							<p className="text-footer">Avenida Apoquindo 3000, Las Condes, Santiago, Chile</p>
+						</div>
+						<div className="info-icons">
+							<i className="fa-solid fa-phone"></i>
+							<p className="text-footer">56 9 1111 2222 / 56 9 2222 3333</p>
+						</div>
+						<div className="info-icons">
+							<i className="fa-solid fa-envelope"></i>
+							<p className="text-footer">contacto@antojapizzeria.cl</p>
+						</div>
+					</div>
+				</div>
 				<div className="information-contacts">
 					<div className="info-icons">
-						<i className="fa-solid fa-location-dot"></i>
-						<p>Avenida Apoquindo 3000, Las Condes, Santiago, Chile</p>
+						<p className="text-footer">Para comentarios y/ sugerencias</p>
 					</div>
 					<div className="info-icons">
-						<i className="fa-solid fa-phone"></i>
-						<p>56 9 1111 2222 / 56 9 2222 3333</p>
-					</div>
-					<div className="info-icons">
-						<i className="fa-solid fa-envelope"></i>
-						<p>contacto@antojapizzeria.cl</p>
+						<Link className="btn btn-dark text-ligth" to="/comment">Escríbenos aquí</Link>
 					</div>
 				</div>
 			</div>
-			<Link className="btn btn-dark text-ligth" to="/comment">Escríbenos</Link>
 		</div>
 	)
 }
--- src/front/components/PhotoPage.jsx
+++ src/front/components/PhotoPage.jsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer"
 import pizza from "../assets/img/pizza.jpg";
 
 export const PhotoPage = () => {
+    const { store } = useGlobalReducer()
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        if (store.token) {
+            navigate("/menu");
+        } else {
+            navigate("/login")
+        }
+    }
 
     return (
         <div className="container-photo p-0 ">
@@ -10,7 +21,10 @@ export const PhotoPage = () => {
                     <h1>50% Chilena, 50% Argentina...100% Sabor </h1>
                 </div>
                 <div className="row col-2 col-md-6">
-                    <Link className="btn btn-menu" to="/menu"><strong>Haz tu pedido</strong></Link>
+                    <button
+                        className="btn btn-menu"
+                        to="/menu"
+                        onClick={handleClick}><strong>Haz tu pedido</strong></button>
                 </div>
             </div>
         </div>
